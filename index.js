592972bd4ca8f3ffe40d4fd3e687a99059f03955
Erdong Feng <edfeng@thoughtworks.com>
2013-11-10 19:58:24 +0800

    modify application to model based behavior.

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ angular.module('myApp', [])
 .controller('myCtrl',
     ['$scope',
     function($scope) {
-      $scope.successMsg = "<strong>Well done!</strong> You successfully read this important alert message.";
-      $scope.infoMsg = "<strong>Heads up!</strong> This alert needs your attention, but it's not super important.";
-      $scope.warningMsg = "<strong>Warning!</strong> Best check yo self, you're not looking too good.";
-      $scope.dangerMsg = "<strong>Oh snap!</strong> Change a few things up and try submitting again.";
-    }
-    ]).directive('alert', [function() {
+      var successMsg = "<strong>Well done!</strong> You successfully read this important alert message.";
+      var infoMsg = "<strong>Heads up!</strong> This alert needs your attention, but it's not super important.";
+      var warningMsg = "<strong>Warning!</strong> Best check yo self, you're not looking too good.";
+      var dangerMsg = "<strong>Oh snap!</strong> Change a few things up and try submitting again.";
+
+      $scope.alerts = [
+        {message: successMsg,
+          type: 'success'
+        },
+        {message: infoMsg,
+          type: 'info'
+        },
+        {message: warningMsg,
+          type: 'warning'
+        },
+        {message: dangerMsg,
+          type: 'danger'
+        },
+      ];
+    }]).directive('alert', [function() {
         return {
           restrict: 'EA',
           replace: true,
