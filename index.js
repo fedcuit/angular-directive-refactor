3493ce8fd55ad158b0a47f4b35150bc43000baea
Erdong Feng <edfeng@thoughtworks.com>
2013-11-10 15:38:26 +0800

    refactor to directive, inherits from parent scope.

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ angular.module('myApp', [])
     function($scope) {
       $scope.successMsg = "<strong>Well done!</strong> You successfully read this important alert message.";
       $scope.infoMsg = "<strong>Heads up!</strong> This alert needs your attention, but it's not super important.";
-      $scope.warningMsg = "";
-      $scope.dangerMsg = "";
+      $scope.warningMsg = "<strong>Warning!</strong> Best check yo self, you're not looking too good.";
+      $scope.dangerMsg = "<strong>Oh snap!</strong> Change a few things up and try submitting again.";
     }
-    ]);
+    ]).directive('alert', [function() {
+        return {
+          restrict: 'EA',
+          template: '<div class="alert alert-success"><div ng-bind-html-unsafe="successMsg"></div></div>',
+          link: function() { }
+        };
+      }]);
 
