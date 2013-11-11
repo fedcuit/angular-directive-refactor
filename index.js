ac87cb3d56a31f99327bbfd713d79c48badedcc4
Erdong Feng <edfeng@thoughtworks.com>
2013-11-11 21:14:45 +0800

    use & to call method on parent scope.

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ angular.module('myApp', [])
 .controller('myCtrl',
     ['$scope',
     function($scope) {
-      $scope.successMsg = "<strong>Well done!</strong> You successfully read this important alert message.";
-      $scope.infoMsg = "<strong>Heads up!</strong> This alert needs your attention, but it's not super important.";
-      $scope.warningMsg = "<strong>Warning!</strong> Best check yo self, you're not looking too good.";
-      $scope.dangerMsg = "<strong>Oh snap!</strong> Change a few things up and try submitting again.";
+      var successMsg = " Well done! You successfully read this important alert message.";
+      var infoMsg = " Heads up! This alert needs your attention, but it's not super important.";
+      var warningMsg = " Warning! Best check yo self, you're not looking too good.";
+      var dangerMsg = " Oh snap! Change a few things up and try submitting again.";
+
+      $scope.alerts = [
+        {
+          message: successMsg,
+          type: 'success'
+        },
+        {
+          message: infoMsg,
+          type: 'info'
+        },
+        {
+          message: warningMsg,
+          type: 'warning'
+        },
+        {
+          message: dangerMsg,
+          type: 'danger'
+        }
+      ];
+      $scope.close = function(index) {
+        console.log(index);
+        $scope.alerts.splice(index, 1);
+      };
     }
     ]).directive('alert', [function() {
         return {
           restrict: 'EA',
-          template: '<div class="alert alert-{{type}}"><div ng-bind-html-unsafe="message"></div></div>',
+          template: '<div class="alert alert-{{type}}"><div ng-bind="message"></div><button type="button" class="close" data-dismiss="alert" aria-hidden="true" ng-click="close()">&times;</button></div>',
+          replace: true,
           scope: {
             message: "@",
-            type: "@"
+            type: "@",
+            close: "&"
           },
           link: function() { }
         };
